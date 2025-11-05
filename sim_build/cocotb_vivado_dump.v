1885a80a84b89441836178e6f809d2bc82747ab4
Roger Fan <rogerjoeyfan@gmail.com>
2025-11-04 22:29:50 -0500

    Delete dumb sim_build files

--- sim_build/cocotb_vivado_dump.v
+++
@@ -1,8 +0,0 @@
-
-`timescale 1ns / 1ps
-module cocotb_vivado_dump();
-  initial begin
-    $dumpfile("/home/fpga/worker_place/temp/temp/e25111aa5cc5494d81746455112089ff/sim_build/tile_painter.fst");
-    $dumpvars(0,tile_painter);
-  end
-endmodule
